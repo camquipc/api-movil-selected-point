ad7dff7b02015166beb736bddc4e45d58c5272c2
Francisco Campos <camqui2011@gmail.com>
2021-12-30 09:31:17 -0400

    merge update from dev branch

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,5 +1,6 @@
 import { Application, Request, Response } from "express";
 
+
 export const loadApiEndpoints = (app: Application): void => {
   app.get("/api", (req: Request, res: Response) => {
     return res.status(200).send({});
