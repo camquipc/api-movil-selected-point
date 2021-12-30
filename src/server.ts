5dfb001955c24da4fd1864e1601b6ecf62623e0c
Francisco Campos <camqui2011@gmail.com>
2021-12-30 09:16:31 -0400

    remove data folder

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import app from "./app";
 
-/**
- * Start Express server.
- */
+
 const server = app.listen(app.get("port"), () => {
   console.log(
     "  App is running at http://localhost:%d in %s mode",
